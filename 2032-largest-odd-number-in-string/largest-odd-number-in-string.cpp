88b57951254a222ff8d2b1140a81b7377a757c38
sourav11012 <70379531+sourav11012@users.noreply.github.com>
2025-07-16 15:56:27 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (37.33%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -6,7 +6,7 @@ public:
         
         for(int i = n - 1 ; i>= 0; i--)
         {
-            if(num[i] % 2 == 1) return num.substr(0, i +1);
+            if((num[i] - '0') % 2 == 1) return num.substr(0, i +1);
 
         }
         return "";

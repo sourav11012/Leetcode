4415a57ee09d3a667ef6499481779e6e023fe6b0
sourav11012 <70379531+sourav11012@users.noreply.github.com>
2025-07-15 21:48:16 -0400

    Time: 19 ms (7.54%) | Memory: 49.2 MB (7.01%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string temp; 
+        int right = 0;
+        int left = 0;
+        int count = 0;
+
+        while(left < s.length())
+        {
+
+
+            if(s[left] == '(') count++;
+            else if(s[left] == ')') count--;
+
+            if(count ==0)
+            {
+                temp = temp + s.substr(right + 1, left - right - 1 );
+                right = left + 1;
+            }
+            left++;
+        }
+        return temp;
+    }
+};

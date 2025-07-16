fad9bdf3225772329d9b114d0e840a6906121643
sourav11012 <70379531+sourav11012@users.noreply.github.com>
2025-07-15 21:49:18 -0400

    Time: 0 ms (100.00%) | Memory: 9 MB (54.01%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string temp; 
+         std::string temp;   // acts like Java's StringBuilder
+
         int right = 0;
-        int left = 0;
+        int left  = 0;
         int count = 0;
 
-        while(left < s.length())
-        {
+        while (left < static_cast<int>(s.length())) {
+            if (s[left] == '(')
+                ++count;
+            else if (s[left] == ')')
+                --count;
 
-
-            if(s[left] == '(') count++;
-            else if(s[left] == ')') count--;
-
-            if(count ==0)
-            {
-                temp = temp + s.substr(right + 1, left - right - 1 );
-                right = left + 1;
+            if (count == 0) {
+                // copy everything between the outermost '(' ... ')'
+                temp.append(s.substr(right + 1, left - right - 1));
+                right = left + 1;    // start of next primitive
             }
-            left++;
+            ++left;
         }
         return temp;
     }

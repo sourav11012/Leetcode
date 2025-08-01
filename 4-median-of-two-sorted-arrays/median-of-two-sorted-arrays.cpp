15d291a42a6ef799a116b13bb6b09557b3a409ac
sourav11012 <70379531+sourav11012@users.noreply.github.com>
2025-08-01 19:51:55 -0400

    Time: 0 ms (100.00%) | Memory: 95.2 MB (44.51%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> merge(nums1.size() + nums2.size());
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if(n2 < n1) return findMedianSortedArrays(nums2,nums1);
 
-        int first = 0;
-        int second = 0;
-        int ptr = 0;
+        int low = 0;
+        int high = n1;
+        int n = n1 + n2;
+        int left = (n1 + n2 +1) >>1;
 
-        while (first < nums1.size() && second < nums2.size()) {
-            if (nums1[first] <= nums2[second]) {
-                merge[ptr] = nums1[first];
-                ptr++;
-                first++;
-            } else {
-                merge[ptr] = nums2[second];
-                ptr++;
-                second++;
+        while(low <= high)
+        {
+            int mid1 = (low + high) >>1;
+            int mid2 = left - mid1;
+
+            int leftMax1 = INT_MIN, leftMax2 = INT_MIN;
+            int rightMin1 = INT_MAX, rightMin2 = INT_MAX;
+            if(mid1 < n1) rightMin1 = nums1[mid1];
+            if(mid2 < n2) rightMin2 = nums2[mid2];
+            if(mid1 - 1 >=0 ) leftMax1 = nums1[mid1 -1];
+            if(mid2 - 1 >= 0) leftMax2 = nums2[mid2 - 1];
+
+            if(leftMax1 <= rightMin2 && leftMax2 <= rightMin1)
+            {
+                if( (n1 + n2) & 1 == 1 ) return max(leftMax1 ,leftMax2);
+                else return (double)(max(leftMax1,leftMax2) + min(rightMin1, rightMin2)) / 2.0;
+            }else if(leftMax1 > rightMin2){ high = mid1 -1;
+                
+            }else{
+                low = mid1 + 1;
             }
         }
-
-        while (first < nums1.size()) {
-            merge[ptr] = nums1[first];
-            ptr++;
-            first++;
-        }
-
-        while (second < nums2.size()) {
-            merge[ptr] = nums2[second];
-            ptr++;
-            second++;
-        }
-
-        int n = merge.size();
-        if (n % 2 == 1) {
-            return (double)merge[n / 2];
-        } else {
-            int fidx = n / 2;
-            int sidx = fidx - 1;
-            return (merge[fidx] + merge[sidx]) / 2.0;
-        }
-
-        return -1; // unreachable, but syntactically valid
+        return -1.0;
     }
 };

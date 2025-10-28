a949edba73e20530b9c65571c93fcbf9faa9662a
sourav11012 <70379531+sourav11012@users.noreply.github.com>
2025-10-27 23:13:33 -0400

    Time: 1 ms (94.00%) | Memory: 42.3 MB (19.75%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    void fn(int start,int end, String s,List<String> wordDict,List<String> curr,List<String> ds )
+    {
+        if(end > s.length() - 1 )
+        {
+            if(start == end )
+            {
+            ds.add(String.join(" ", curr));
+            return;
+            }
+            else{
+                return;
+            }
+        }
+
+        if(wordDict.contains(s.substring(start, end + 1)))
+        {
+            curr.add(s.substring(start, end + 1));
+            
+            fn(end + 1, end + 1, s, wordDict, curr, ds);
+            curr.remove(curr.size() - 1);
+        }
+        
+        
+        fn(start, end + 1, s, wordDict, curr, ds);
+        
+        return ;
+    }
+
+
+
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        // StringBuilder sb = new StringBuilder();
+        List<String> curr = new ArrayList<>();
+        List<String> ds = new ArrayList<>();
+
+        fn(0,0,s,wordDict,curr,ds);
+        // List<String> result = new ArrayList<>();
+        // for(String temp : ds)
+        // {
+        //     if(temp.equals(""))
+        //     continue;
+        //     else
+        //     result.add(temp);
+        // }
+        return ds;
+    }
+}

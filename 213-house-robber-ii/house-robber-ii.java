fa9598183d41cf3c5059633fde14a3c51885b3e3
sourav11012 <70379531+sourav11012@users.noreply.github.com>
2025-11-24 20:53:17 -0500

    Time: 0 ms (100.00%) | Memory: 42.9 MB (12.66%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,21 +1,21 @@
 class Solution {
-     public int rob1(ArrayList<Integer> nums) {
-        int n = nums.size();
+      public int rob1(int[] nums) {
+        int n = nums.length;
 
         if(n == 0) return 0;
-        if(n == 1) return nums.get(0);
-        if(n == 2) return Math.max(nums.get(0), nums.get(1));
+        if(n == 1) return nums[0];
+        if(n == 2) return Math.max(nums[0], nums[1]);
 
-        int dp[] = new int[n];
+        int dp[] = new int[nums.length];
         // dp[0] = nums[0];
         // dp[1] = Math.max(nums[0], nums[1]);
-        int prev2 = nums.get(0);
-        int prev1 = Math.max(nums.get(0), nums.get(1));
+        int prev2 = nums[0];
+        int prev1 = Math.max(nums[0], nums[1]);
         // Arrays.fill(dp, -1);
         for(int i = 2 ; i< n;i++)
         {
            
-            int take = nums.get(i) + prev2;
+            int take = nums[i] + prev2;
             int notTake = prev1;
             int curr = Math.max(take, notTake);
             prev2 = prev1;
@@ -24,14 +24,11 @@ class Solution {
         return prev1;
     }
     public int rob(int[] nums) {
-        ArrayList<Integer> firstIn= new ArrayList<>();
-        ArrayList<Integer> lastIn = new ArrayList<>();
-        if(nums.length == 1) return nums[0];
-        for(int i = 0 ;i <nums.length;i++)
-        {
-            if(i != 0) lastIn.add(nums[i]);
-            if( i != nums.length - 1) firstIn.add(nums[i]);
-        }
+       if(nums.length ==1 ) return nums[0];
+        int[] lastIn = Arrays.copyOfRange(nums, 1, nums.length);
+        int[] firstIn = Arrays.copyOfRange(nums, 0, nums.length-1);
+
+        
         return Math.max(rob1(firstIn), rob1(lastIn));
     }
 }

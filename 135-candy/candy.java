b79f7c29248a75b139ada3550ea14920f40ee5e2
sourav11012 <70379531+sourav11012@users.noreply.github.com>
2025-12-04 13:07:53 -0500

    Time: 3 ms (85.09%) | Memory: 47.9 MB (5.97%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,32 +1,43 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int[] leftCandies = new int[n];
-        int[] rightCandies = new int[n];
+        int i = 1;
+        int sum =1;
+        int peak = 1;
+        int down = 1;
+        while(i < n)
+        {
+            if(ratings[i] == ratings[i-1])
+            {
+             sum+=1;
+            
+             i++;
+              continue;
+            } 
+            peak = 1;
 
-        Arrays.fill(leftCandies, 1);
-        Arrays.fill(rightCandies, 1);
-
-        // LEFT → RIGHT
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                leftCandies[i] = leftCandies[i - 1] + 1;
+            while(i < n && ratings[i] > ratings[i-1])
+            {
+                peak = peak + 1;
+                sum = sum + peak;
+                i++;
             }
-        }
-
-        // RIGHT → LEFT
-        for (int j = n - 2; j >= 0; j--) {
-            if (ratings[j] > ratings[j + 1]) {
-                rightCandies[j] = rightCandies[j + 1] + 1;
+            down = 1;
+            while(i < n && ratings[i] < ratings[i-1])
+            {
+                
+                sum = sum + down;
+                down = down + 1;
+                i++;
             }
-        }
+            if(down > peak)
+            {
+                sum = sum + (down - peak);
+            }
+            
 
-        // FINAL SUM
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += Math.max(leftCandies[i], rightCandies[i]);
-        }
 
+        }
         return sum;
     }
 }

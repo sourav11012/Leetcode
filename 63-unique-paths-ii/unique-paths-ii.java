e911a46a4e04f6335f2e838900b580271b4cf58a
sourav11012 <70379531+sourav11012@users.noreply.github.com>
2025-12-07 18:47:28 -0500

    Time: 0 ms (100.00%) | Memory: 43.5 MB (22.09%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -4,7 +4,7 @@ class Solution {
     {
         int n = grid.length;
         int m = grid[0].length;
-         if(i < 0 || j < 0 || grid[i][j] == 1 ) return 0;
+        if(i < 0 || j < 0 || grid[i][j] == 1 ) return 0;
         if(i == 0 && j == 0) return 1;
        
         if(dp[i][j] != -1) return dp[i][j];
@@ -21,10 +21,27 @@ class Solution {
         int m = obstacleGrid[0].length;
         int dp[][] = new int[n][m];
 
-        for(int[] temp : dp)
+        // for(int[] temp : dp)
+        // {
+        //     Arrays.fill(temp,-1);
+        // }
+
+        for(int i = 0;i<n;i++)
         {
-            Arrays.fill(temp,-1);
+            for(int j = 0;j<m;j++)
+            {
+                if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else if(i == 0 && j == 0) dp[i][j] = 1;
+                else{
+                    int left = 0; int up = 0;
+                    if(j > 0) left = dp[i][j -1];
+                    if(i > 0) up = dp[i-1][j];
+                    dp[i][j] = left+ up;
+                }
+                    
+            }
         }
-        return fn(obstacleGrid,n-1, m-1,dp);
+        // return fn(obstacleGrid,n-1, m-1,dp);
+       return  dp[n-1][m-1];
     }
 }

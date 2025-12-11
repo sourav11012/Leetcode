102461e4e910b67d6b31bc9c37ea437a01c0d7d8
sourav11012 <70379531+sourav11012@users.noreply.github.com>
2025-12-10 20:27:23 -0500

    Time: 0 ms (100.00%) | Memory: 46.7 MB (57.12%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -14,41 +14,27 @@
  * }
  */
 class Solution {
+    int maxDiameter = Integer.MIN_VALUE;
     public int findHeight(TreeNode curr)
     {
         if(curr == null)
         {
             return 0;
         }
-        return Math.max(findHeight(curr.left), findHeight(curr.right))  +1;
-    }
+        int left = findHeight(curr.left);
+        int right = findHeight(curr.right);
+        maxDiameter = Math.max(left + right, maxDiameter);
 
-    void traverse(Queue<TreeNode> q , TreeNode root)
-    {
-        if(root == null) return;
-
-        traverse(q, root.left);
-        q.offer(root);
-        traverse(q, root.right);
-
+        return Math.max(left,right)  +1;
     }
 
     public int diameterOfBinaryTree(TreeNode root) {
         
         if(root == null) return 0;
-        Queue<TreeNode> q = new LinkedList<>();
+        findHeight(root);
+       
 
-        traverse(q, root);
-        int maxDiameter = Integer.MIN_VALUE;
-            while(!q.isEmpty())
-            {
-            TreeNode curr = q.poll();
-            int left = findHeight(curr.left);
-            int right = findHeight(curr.right);
-            maxDiameter = Math.max(left + right, maxDiameter);
-            }
-
-            return maxDiameter;
+        return maxDiameter;
            
      
        

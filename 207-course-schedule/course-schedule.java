a410f6bbbd5492272eefa0fadd676d148d452b43
sourav11012 <70379531+sourav11012@users.noreply.github.com>
2025-12-14 20:08:18 -0500

    Time: 7 ms (57.97%) | Memory: 47 MB (44.30%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -3,26 +3,25 @@ class Solution {
     // 1 -> cycle detected
     // 2 -> completely visited
 
-    public boolean findCycle(int curr,List<List<Integer>> adj, int[]visited )
-    {
-        if(visited[curr] == 1) return false; // cycle detected
+    // public boolean findCycle(int curr,List<List<Integer>> adj, int[]visited )
+    // {
         
-        visited[curr] = 1;
-        for(int neig : adj.get(curr))
-        {
-            if(visited[neig] == 1) return false;
+    //     visited[curr] = 1;
+    //     for(int neig : adj.get(curr))
+    //     {
+    //         if(visited[neig] == 1) return false;  // cycle detected
 
-            if(visited[neig] == 0)
-            {
-                if (!findCycle(neig, adj, visited)) {
-                    return false;
-                }
-            }
-        }
-        visited[curr] = 2;
-        return true;
+    //         if(visited[neig] == 0)
+    //         {
+    //             if (!findCycle(neig, adj, visited)) {
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     visited[curr] = 2;
+    //     return true;
 
-    }
+    // }
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList<>();
@@ -35,15 +34,41 @@ class Solution {
         {
             adj.get(edge[1]).add(edge[0]);
         }
-        int[] visited = new int[numCourses];
+        int[] indegree = new int[numCourses];
 
         for (int i = 0; i < numCourses; i++) {
-            if (visited[i] == 0) {
-                if (!findCycle(i, adj, visited)) {
-                    return false;
+            for(int it : adj.get(i))
+            {
+                indegree[it]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0;i< indegree.length;i++)
+        {
+            if(indegree[i] == 0)
+            {
+                q.add(i);
+            }
+        }
+        ArrayList<Integer> topo =  new ArrayList<>();
+
+        while(!q.isEmpty())
+        {
+            int currNode = q.poll();
+            if(indegree[currNode] == 0 ) {
+                topo.add(currNode);
+            }
+            
+            for(int neigh : adj.get(currNode))
+            {
+                indegree[neigh]--;
+                if(indegree[neigh] == 0) {
+                    q.add(neigh);
                 }
             }
         }
-        return true;
+
+
+        return topo.size() == numCourses;
     }
 }

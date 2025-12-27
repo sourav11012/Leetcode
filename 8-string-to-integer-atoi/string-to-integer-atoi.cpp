3d4f9db5351b1271ab82ca834b247905c1d7063f
sourav11012 <70379531+sourav11012@users.noreply.github.com>
2025-12-26 19:47:26 -0500

    Time: 0 ms (100.00%) | Memory: 9 MB (97.12%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
+    int fn(const string &s, int i, long long num, int sign)
+    {
+        if(i >= s.size() || !isdigit(s[i]))
+        {
+            return (int)(sign * num);
+        }
+        num = num * 10 + (s[i] - '0');
+
+        if(sign * num >= INT_MAX) return INT_MAX;
+        if(sign * num <= INT_MIN) return INT_MIN;
+
+        return fn(s, i+1, num, sign);
+    }
     int myAtoi(string s) {
-        int i = 0, n = s.size();
-        // Skip leading whitespaces
-        while (i < n && s[i] == ' ') i++;
-
-        // Handle sign
+        int i =0;
+        while(i < s.size() && s[i] == ' ')
+        {
+            i++;
+        }
         int sign = 1;
-        if (i < n && (s[i] == '+' || s[i] == '-')) {
-            if (s[i] == '-') sign = -1;
+        if(i < s.size() && (s[i] == '+' || s[i] == '-'))
+        {
+            sign = s[i] == '+' ? 1 : -1;
             i++;
         }
-
-        // Parse digits
-        long num = 0;
-        while (i < n && isdigit(s[i])) {
-            num = num * 10 + (s[i] - '0');
-            if (sign * num > INT_MAX) return INT_MAX;
-            if (sign * num < INT_MIN) return INT_MIN;
-            i++;
-        }
-
-        return (int)(sign * num);
+        return fn(s,i,0,sign);
     }
 };

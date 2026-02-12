63ebc84f54125f3863ba2caed4359c6214b90ce6
sourav11012 <70379531+sourav11012@users.noreply.github.com>
2026-02-12 00:39:51 -0500

    Time: 74 ms (6.14%) | Memory: 48 MB (55.02%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,26 +1,38 @@
-import java.util.*;
-
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        if (hand.length % groupSize != 0) return false;
+        int n = hand.length;
+        if(n % groupSize != 0) return false;
+        
 
-        TreeMap<Integer, Integer> freq = new TreeMap<>();
-        for (int card : hand) {
-            freq.put(card, freq.getOrDefault(card, 0) + 1);
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        for(int i : hand)
+        {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
+        
+        while(!map.isEmpty())
+        {
+            
+            int start = map.firstKey();
 
-        for (int key : freq.keySet()) {
-            int count = freq.get(key);
-            if (count > 0) {
-                // Try to form group starting from key
-                for (int i = 0; i < groupSize; i++) {
-                    int curr = key + i;
-                    if (!freq.containsKey(curr) || freq.get(curr) < count)
-                        return false; // cannot form a valid straight group
-                    freq.put(curr, freq.get(curr) - count);
+            int count = map.get(start);
+
+            for(int i =0;i<groupSize; i++)
+            {
+                int num = start + i;
+
+                if(!map.containsKey(num) || map.get(num) < count) return false;
+
+                map.put(num, map.get(num) - 1);
+                if(map.get(num) == 0) {
+                    map.remove(num);
                 }
             }
-        }
-        return true;
+
+        }   
+        return map.isEmpty();
+        
+        // [1,2,2,3,3,4,6,7,8]
     }
 }

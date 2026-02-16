19da386238dd56e45495c7f206b3571decabe7ff
sourav11012 <70379531+sourav11012@users.noreply.github.com>
2026-02-16 00:03:08 -0500

    Time: 64 ms (17.08%) | Memory: 42.8 MB (99.55%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,53 +1,37 @@
 class Solution {
 
-    private int lcs(String s1, String s2, int idx1, int idx2,int[][] dp)
-
+    private int lcs(String s, String reversed)
     {
-        if(idx1 < 0 || idx2 < 0)
-        {
-            return 0;
-        }
-        
-        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        int prev[] = new int[s.length() + 1];
+        int curr[] = new int[s.length() + 1];
 
-        if(s1.charAt(idx1) == s2.charAt(idx2))
+        for(int idx1 = 1 ; idx1 <= s.length(); idx1++)
         {
-            dp[idx1][idx2] =  1 + lcs(s1, s2, idx1-1, idx2-1,dp);
-            return dp[idx1][idx2];
+            for(int idx2 = 1 ; idx2 <= reversed.length() ; idx2++)
+            {
+                if(s.charAt(idx1 - 1) == reversed.charAt(idx2 - 1))
+                {
+                    curr[idx2] = 1 + prev[idx2-1];
+                }
+                else {
+                    curr[idx2] = Math.max(prev[idx2], curr[idx2-1]);
+                }
+            }
+
+            // swap arrays
+            int[] temp = prev;
+            prev = curr;
+            curr = temp;
+
+            Arrays.fill(curr, 0);
         }
 
-        dp[idx1][idx2] =  Math.max(lcs(s1, s2, idx1-1, idx2,dp), lcs(s1, s2, idx1 , idx2-1,dp));
-       
-        return dp[idx1][idx2];
+        return prev[s.length()];
     }
 
     public int longestPalindromeSubseq(String s) {
-        
+
         String reversed = new StringBuilder(s).reverse().toString();
-        int dp[][]= new int[s.length() + 1][s.length() + 1];
-
-
-        for(int idx1 = 0 ; idx1 <= s.length(); idx1++)
-        {
-            for(int idx2 = 0 ; idx2 <= reversed.length() ; idx2++)
-            {
-                if(idx1 == 0 || idx2==0)
-                {
-                   
-                    continue;
-                }
-                
-                 if(s.charAt(idx1 - 1) == reversed.charAt(idx2 - 1))
-                {
-                    dp[idx1][idx2] =  1 + dp[idx1-1][ idx2-1];
-                    
-                }
-                else dp[idx1][idx2] =  Math.max(dp[idx1-1][idx2], dp[idx1] [idx2-1]);
-            
-                
-            }
-        }
-        
-        return dp[s.length()][ s.length()];
+        return lcs(s, reversed);
     }
 }

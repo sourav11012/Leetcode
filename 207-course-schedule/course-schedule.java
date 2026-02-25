f6f0b81f991f81d22259a6302812bbc15173d5db
sourav11012 <70379531+sourav11012@users.noreply.github.com>
2026-02-24 23:31:32 -0500

    Time: 7 ms (61.32%) | Memory: 47.2 MB (65.84%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,51 +1,97 @@
 class Solution {
 
-    boolean dfs(int curr, int[] visited, List<List<Integer>> adj)
-    {
+    // boolean dfs(int curr, int[] visited, List<List<Integer>> adj)
+    // {
 
-        if(visited[curr] == 1) return true;
-        if(visited[curr] == 2) return false;
+    //     if(visited[curr] == 1) return true;
+    //     if(visited[curr] == 2) return false;
 
-        visited[curr] = 1;
+    //     visited[curr] = 1;
 
-        for(int neigh : adj.get(curr))
-        {
+    //     for(int neigh : adj.get(curr))
+    //     {
             
 
            
-                if(dfs(neigh, visited, adj)){
-                    return true;
-                }
+    //             if(dfs(neigh, visited, adj)){
+    //                 return true;
+    //             }
             
-        }
-        visited[curr] =2;
-        return false;
+    //     }
+    //     visited[curr] =2;
+    //     return false;
 
-    }
+    // }
 
 
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj =new ArrayList<>();
-        for(int i = 0;i <numCourses; i++)
+
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < numCourses ; i++)
         {
             adj.add(new ArrayList<>());
         }
-
-        for(int [] edge : prerequisites)
+        for(int[] edge : prerequisites)
         {
             adj.get(edge[1]).add(edge[0]);
+            indegree[edge[0]]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 0; i< numCourses; i++)
+        {
+           
+            if(indegree[i] == 0)
+            {
+                q.offer(i);
+            }
+        }
+        List<Integer> topo = new ArrayList<>();
+
+        while(!q.isEmpty())
+        {
+            int curr = q.poll();
+            topo.add(curr);
+
+           
+            for(int neig : adj.get(curr))
+            {
+                indegree[neig]--;
+                if(indegree[neig] == 0) {
+                    q.offer(neig);
+                    
+                }
+            }
         }
 
-        int[] visited = new int[numCourses];
-        for(int i = 0 ;i < numCourses; i++)
-        {
-            if(dfs(i,visited, adj)){
-            return false;
-        }
-        }
+        return topo.size() == numCourses;
         
 
-        return true;
+
+
+
+        // List<List<Integer>> adj =new ArrayList<>();
+        // for(int i = 0;i <numCourses; i++)
+        // {
+        //     adj.add(new ArrayList<>());
+        // }
+
+        // for(int [] edge : prerequisites)
+        // {
+        //     adj.get(edge[1]).add(edge[0]);
+        // }
+
+        // int[] visited = new int[numCourses];
+        // for(int i = 0 ;i < numCourses; i++)
+        // {
+        //     if(dfs(i,visited, adj)){
+        //     return false;
+        // }
+        // }
+        
+
+        // return true;
     }
 }
